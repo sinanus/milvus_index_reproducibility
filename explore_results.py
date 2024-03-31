edb47bb5d6ad40b3f75a88fb1208d3faaef2d5d1
Sinan Karaveli <sinankaraveli@gmail.com>
2024-03-31 18:50:26 -0400

    cleaned up analysis script. updated results in readme

--- explore_results.py
+++ explore_results.py
@@ -4,15 +4,17 @@ import glob
 import pickle
 import pandas as pd
 import numpy as np
-import time
-
+import time 
+import itertools
 # load latests file
 
 files = glob.glob('all_test_results*.pickle')
 latest_file = max(files, key=os.path.getctime)
 run_uuid = latest_file.split('uuid_')[-1].split('.pickle')[0]
+
 print(f'Analysing results from run: {run_uuid} from  {time.ctime(os.path.getctime(latest_file))}')
-files = sorted(glob.glob(f'*{run_uuid}*.pickle'))
+
+files = glob.glob(f'*{run_uuid}*.pickle')
 
 all_test_results = []
 for f in files:
@@ -37,27 +39,45 @@ def compare_two_hits_dict(hits0:dict, hits1: dict):
 		n = len(hits0.values()) if len(hits0.values())<len(hits1.values()) else len(hits1.values())
 		ordered_keys_value_delta = [hits0.values()[i] - hits1.values()[i] for i in range(n)]
 	ordered_keys_MAE = np.mean(np.abs(ordered_keys_value_delta))
-	return ordered_keys_MAE, n_intersecting_keys, intersecting_keys_value_deltas
+
+	output = dict(zip(
+			['MAE_returned_vector_distances', 'n_vectors_returned_in_both_search', 'distance_delta_for_vecs_returned_in_both_search'],
+			 [ordered_keys_MAE, n_intersecting_keys, intersecting_keys_value_deltas]
+			 ))
+	if (n_intersecting_keys < len(hits0.keys())) or (n_intersecting_keys < len(hits1.keys())):
+		output.update({'hits0':str(hits0),
+				 		'hits1':str(hits1)})
+	return output
 
 
 
 df_metadata = pd.DataFrame([pd.Series(x['metadata']) for x in all_test_results])
 # cols_highlight = df.columns[df.apply(lambda x: len(x.apply(str).unique()),axis=0)>1]
 # cols_highlight = [c for c in cols_highlight if 'uuid' not in c]
-cols_highlight = ['n_vec_index','n_vec_search','single_non_vector_field','results_reproducible']
-df_metadata = df_metadata.sort_values(['n_vec_index','single_non_vector_field'])
+cols_highlight = ['n_vec_index','n_vec_search','results_reproducible']
+df_metadata = df_metadata.sort_values(['n_vec_index'])
 print(df_metadata[cols_highlight])
 
 reproducibility_fractions = []
+
+detailed_results_comparison_rebuilt_index = []
+detailed_results_comparison_same_index = []
+
 delta_between_different_indices = []
+delta_different_indices_same_embeddings_distance_difference = []
+delta_MAE_returned_vector_distances = []
+
 for i, test_res in enumerate(all_test_results):
 	res_df = pd.DataFrame(test_res['results'])
 
+	# Check complete Reproducibility for same and rebuilt (different) index
+	## same_idx reproducibility: repeated queries on the same index (without rebuilding) for all the rebuilds. 
 	same_idx = res_df.groupby(['index_rebuild_id','search_vectors_batch_id','search_vector_within_batch_id'])['res']\
         .apply(lambda x: all_list_elements_identical(x.to_list()))\
      .value_counts(normalize=True)
 	same_idx.name = 'same_index_search_reproducibility'
 
+	## different_idx reproducibilit for rebuilt (with identical vectors and settings) for all of repeat searches
 	different_idx = res_df.groupby(['search_vectors_batch_id','search_vector_within_batch_id'])['res']\
         .apply(lambda x: all_list_elements_identical(x.to_list()))\
      .value_counts(normalize=True)
@@ -72,41 +92,101 @@ for i, test_res in enumerate(all_test_results):
 	sdf['test_uuid'] = test_res['metadata']['test_uuid']
 	reproducibility_fractions.append(sdf)
 
-	delta_results = []
-	for g, ssdf in res_df.groupby(['search_vectors_batch_id','search_vector_within_batch_id'])['res']:
-		delta_results.append(compare_two_hits_dict(ssdf.iloc[0],ssdf.iloc[-1]))
-	delta_results = pd.DataFrame(data=delta_results,columns = ['MAE_returned_vector_distances', 'n_vectors_returned_in_both_search', 'disntace_delta_for_vecs_returned_in_both_search'])
-	delta_between_different_indices.append(delta_results.n_vectors_returned_in_both_search.value_counts())
+	# check detailed comparisons for same index and rebuilt index
+	## same index
+	same_idx_detailed = []
+	group_cols = ['index_rebuild_id','search_vectors_batch_id','search_vector_within_batch_id']
+	for group_vals, ssdf in res_df.groupby(group_cols)['res']:
+		for (a,b) in itertools.combinations(range(ssdf.shape[0]),2):
+			r = compare_two_hits_dict(ssdf.iloc[a],ssdf.iloc[b])
+			r.update(dict(zip(group_cols,group_vals)))
+			same_idx_detailed.append(r)
+
+	different_idx_detailed = []
+	group_cols = ['same_index_search_repeat_id','search_vectors_batch_id','search_vector_within_batch_id']
+	for group_vals, ssdf in res_df.groupby(group_cols)['res']:
+		for (a,b) in itertools.combinations(range(ssdf.shape[0]),2):
+			r = compare_two_hits_dict(ssdf.iloc[a],ssdf.iloc[b])
+			r.update(dict(zip(group_cols,group_vals)))
+			different_idx_detailed.append(r)
+
+	
+	detailed_results_comparison_same_index.append(pd.DataFrame(same_idx_detailed))
+	detailed_results_comparison_rebuilt_index.append(pd.DataFrame(different_idx_detailed))
+
 
 reproducibility_fractions = pd.concat(reproducibility_fractions).reset_index().pivot(index='test_uuid',values=True,columns='index')
 test_uuids = [x['metadata']['test_uuid'] for x in all_test_results]
-delta_between_different_indices_df = pd.DataFrame(delta_between_different_indices)
-delta_between_different_indices_df.index = test_uuids
-delta_between_different_indices_df.columns = pd.MultiIndex.from_tuples([(delta_between_different_indices_df.columns.name,c) for c in delta_between_different_indices_df])
+
+for test_id, res in zip(test_uuids,detailed_results_comparison_same_index):
+	try:
+		res_stats_df = pd.DataFrame(
+										data = res.MAE_returned_vector_distances\
+												.describe().loc[['min','mean','max']]\
+												.rename(lambda x: 'Repeat_search_on_SAME_index\nMAE_distances_'+x)\
+												.rename(test_id)
+												).T
+		res_stats_df = res_stats_df.join(
+										pd.DataFrame(
+										data = res.n_vectors_returned_in_both_search\
+												.value_counts()\
+												.rename(lambda x: 'Repeat_search_on_SAME_index\nnum_vecs_in_topK_in_both_search_-_'+str(x))\
+												.rename(test_id)
+												).T
+
+										)
+		col_overlap = sorted(list(set(reproducibility_fractions.columns.to_list()).intersection(res_stats_df.columns.to_list())))
+		col_diff = sorted(list(set(res_stats_df.columns.to_list()).difference(reproducibility_fractions.columns.to_list())))
+		if len(col_overlap)>0:
+			reproducibility_fractions.update(res_stats_df[col_overlap])
+		if len(col_diff)>0:
+			reproducibility_fractions = reproducibility_fractions.join(res_stats_df[col_diff])
+
+	except:
+		import pdb
+		pdb.set_trace()
+
+
+for test_id, res in zip(test_uuids,detailed_results_comparison_rebuilt_index):
+	try:
+		res_stats_df = pd.DataFrame(
+										data = res.MAE_returned_vector_distances\
+												.describe().loc[['min','mean','max']]\
+												.rename(lambda x: 'Repeat_search_on_REBUILT_indices\nMAE_distances_'+x)\
+												.rename(test_id)
+												).T
+		res_stats_df = res_stats_df.join(
+										pd.DataFrame(
+										data = res.n_vectors_returned_in_both_search\
+												.value_counts()\
+												.rename(lambda x: 'Repeat_search_on_REBUILT_indices\nnum_vecs_in_topK_in_both_search_-_'+str(x))\
+												.rename(test_id)
+												).T
+
+										)
+		col_overlap = sorted(list(set(reproducibility_fractions.columns.to_list()).intersection(res_stats_df.columns.to_list())))
+		col_diff = sorted(list(set(res_stats_df.columns.to_list()).difference(reproducibility_fractions.columns.to_list())))
+		if len(col_overlap)>0:
+			reproducibility_fractions.update(res_stats_df[col_overlap])
+		if len(col_diff)>0:
+			reproducibility_fractions = reproducibility_fractions.join(res_stats_df[col_diff])
+
+	except:
+		import pdb
+		pdb.set_trace()
+	
 df_summary_results = df_metadata.set_index('test_uuid').join(reproducibility_fractions)
 df_summary_results = df_summary_results.rename(columns={'same_index_search_reproducibility':'fraction_reproducible_search_on_SAME_index',
 						 						'different_index_search_reproducibility':'fraction_reproducible_search_on_REBUILT_index'})
-df_summary_results.columns = pd.MultiIndex.from_arrays([['']*len(df_summary_results.columns),df_summary_results.columns])
-df_summary_results = df_summary_results.join(delta_between_different_indices_df)
 
-
-cols_highlight = [('','n_vec_index'),
-				  ('','n_vec_search'),
-				  ('','single_non_vector_field'),
-				  ('','results_reproducible'),
-				  ('','fraction_reproducible_search_on_SAME_index'),
-				  ('','fraction_reproducible_search_on_REBUILT_index'),
-				  ('n_vectors_returned_in_both_search', 0),
-				('n_vectors_returned_in_both_search', 1),
-				('n_vectors_returned_in_both_search', 2),
-				('n_vectors_returned_in_both_search', 3)]
-
-cols_highlight = [c for c in cols_highlight if c in df_summary_results.columns]
+cols_highlight = [c for c in df_summary_results.columns if 'uuid' not in c]
 
 if df_summary_results.shape[0]<5:
 	df_summary_results_display = df_summary_results[cols_highlight].T\
 								.to_markdown(index=True)
 else:
-	df_summary_results_display = df_summary_results[cols_highlight]\
+	df_summary_results_display = df_summary_results[cols_highlight].rename(columns={k:k.replace('\n',':') for k in cols_highlight})\
 								.to_markdown(index=False)
 print(df_summary_results_display)
+with open(f'analysis_summary_run_uuid_{run_uuid}.md','w') as f:
+	f.write(df_summary_results_display)
